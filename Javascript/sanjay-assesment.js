ba315d7c2683709cb1e04117254116149aa055e5
JBSanjay <jbsanjaysanjay579@gmail.com>
2022-08-01 15:39:09 +0530

    added code to fetch data from api

--- Javascript/sanjay-assesment.js
+++ Javascript/sanjay-assesment.js
@@ -53,10 +53,48 @@ function display() {
         <div>`
     });
     document.getElementById("fetch").innerHTML = detail;
-
 }
 
+function search() {
+    let value = document.getElementById("searchbars").value;
+    let temp = " ";
+    let storage = JSON.parse(localStorage.getItem("Staffinfo"));
+    console.log(storage);
 
+    let resvalue = storage.filter(obj => obj.name.first === value);
+    console.log(resvalue);
+    var detail2 = '';
+    for (var i = 0; i < resvalue.length; i++) {
+        detail2 = `
+            <div class="display">
+            <img src=${storage.picture.large}>
+            <span class="details">
+            <p>Name:${storage.name.title} ${storage.name.first}</p>
+            <p >Email:${storage.email}</p>
+            <p>Gender:${storage.gender}</p>
+            </span>
+           <div>`
+    }
+    document.getElementById("search1").innerHTML = detail2;
+    // temp = storage.foreach(myfunction);
+    // myfunction();
+
+    // function myfunction() {
+    //     if (value == Staffinfo.name.first) {
+    //         detail2 = `
+    //         <div class="display">
+    //         <img src=${value.picture.large}>
+    //         <span class="details">
+    //         <p>Name:${value.name.title} ${value.name.first}</p>
+    //         <p >Email:${value.email}</p>
+    //         <p>Gender:${value.gender}</p>
+    //         </span>
+    //         <div>`
+    //     }
+    //     document.getElementById("fetch").innerHTML = detail2;
+    // }
+
+}
 // function locstorage(sdata) {
 //     locstorage.push(sdata);
 //     localStorage.setitem("Staffinfo", JSON.stringify(sdata));
